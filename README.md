84f947e8cf78354cd6a64140b1b89d7b172923dd
Kostis Gourgoulias <kgourgoulias@gmail.com>
2024-02-28 21:38:04 +0000

    new

---
+++ README.md
@@ -0,0 +1,8 @@
+# raggler_dev
+
+
+My own experiments with RAG
+
+# Installing 
+
+Please use `pip3 install -e .` to install this code. 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My own experiments with RAG',
+    author='Kostis Gourgoulias',
+    license='MIT',
+)
