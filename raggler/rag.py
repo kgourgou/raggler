38b53e4226432a2a0f928f6d23b5dcb8cb9cfce6
Kostis Gourgoulias <kgourgoulias@gmail.com>
2024-03-10 12:04:17 +0000

    more tests

--- raggler/rag.py
+++ raggler/rag.py
@@ -52,7 +52,7 @@ def create_index(
     logger.info("paths_to_directories", paths_to_directories)
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500, chunk_overlap=0.5, length_function=len
+        chunk_size=300, chunk_overlap=0.5, length_function=len
     )
 
     all_content = []
--- tests/test_rag.py
+++ tests/test_rag.py
@@ -1,6 +1,59 @@
-class DummyEmbedder:
+import numpy as np
+from raggler import RAG, create_index
+from raggler.rag import EXT_TO_LOADER
+from raggler.base_classes.base_classes import BaseIndex, BaseLLM
+
+
+class MockEmbedder:
     def __init__(self, *args, **kwargs):
         pass
 
     def encode(self, content):
-        return
+        return np.array([1, 1, 1, 1])
+
+
+class MockIndex(BaseIndex):
+    def __init__(self, *args, **kwargs):
+        self.content = None
+        self.index = None
+
+    def add(self, vectors, content):
+        self.content = content
+        self.index = vectors
+
+    def retrieve(self, query_embedding, k: int = 1):
+        return np.array([0]), np.array([0])
+
+    def save(self, path_to_save_index: str):
+        pass
+
+    def load(self, path_to_index: str):
+        pass
+
+
+class MockMLM(BaseLLM):
+    def __call__(self, **kwargs) -> str:
+        return "response"
+
+
+def test_rag():
+    index = MockIndex()
+    index.content = ["content"]
+    index.index = np.array([1, 1, 1, 1])
+
+    rag = RAG(MockEmbedder(), index, MockMLM())
+    assert rag("query") == "response"
+    assert rag.retrieve("query") == (np.array([0]), np.array([0]))
+
+
+def test_create_index():
+    index = create_index(
+        paths_to_directories=["tests/test_index/"],
+        embedder=MockEmbedder(),
+        index=MockIndex(),
+    )
+    assert index.content == ["content"]
+
+
+def test_loader():
+    assert len(EXT_TO_LOADER) > 0
